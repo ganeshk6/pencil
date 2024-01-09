fa8d7477854b374c9f24428f79e0072853e20c32
Ganesh Kumar <ganesh620671@gmail.com>
2024-01-09 13:18:20 +0530

    pencil

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Feature from "./Components/Features/Feature";
 import About from "./Components/About/About";
-import Pencil from "./Components/Pencil/pencil";
+import Pencil from "./Components/Pencil/Pencil";
 import VideoComponents from "./Components/Video/Video";
 
 
